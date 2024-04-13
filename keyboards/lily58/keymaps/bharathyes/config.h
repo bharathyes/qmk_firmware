3f814d45810fc69143959a687109ae87d88b4f8e
bharathyes <bharathsoftdev@gmail.com>
2024-04-13 16:35:29 +0530

    Layer Overhaul (extensive)

--- keyboards/lily58/keymaps/bharathyes/config.h
+++ keyboards/lily58/keymaps/bharathyes/config.h
@@ -20,39 +20,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* Select hand configuration */
 
+// ways to set the master side 
 #define MASTER_LEFT
 // #define MASTER_RIGHT
+// https://docusaurus.qmk.fm/feature_split_keyboard#handedness-by-eeprom
 // #define EE_HANDS
 
 #define QUICK_TAP_TERM 0
-// #define TAPPING_TERM 100
 
 #undef RGBLED_NUM
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_SNAKE
-#define RGBLIGHT_EFFECT_KNIGHT
-#define RGBLIGHT_EFFECT_CHRISTMAS
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_EFFECT_ALTERNATING
-#define RGBLIGHT_EFFECT_TWINKLE
-#define RGBLED_NUM 27
-#define RGBLIGHT_LIMIT_VAL 120
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
-
-// Underglow
-/*
-#undef RGBLED_NUM
-#define RGBLED_NUM 14    // Number of LEDs
-#define RGBLIGHT_SLEEP
-*/
-
 
 #define GRAVE_ESC_GUI_OVERRIDE
 
@@ -64,16 +41,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 200
 // #define TAPPING_TERM_PER_KEY
 #define PERMISSIVE_HOLD_PER_KEY
-// #define HOLD_ON_OTHER_KEY_PRESS
+
+// #define HOLD_ON_OTHER_KEY_PRESS  
+//  conflicts with permissive hold
 
 // exploring 
 // #define IGNORE_MOD_TAP_INTERRUPT
 // #define PERMISSIVE_HOLD 
 
 
-
-#define ONESHOT_TAP_TOGGLE 4 /* tapping said times holds key until tapped once again */
-#define ONESHOT_TIMEOUT 2500 /* time (in ms) befoer the one shot key is released */
+#define ONESHOT_TAP_TOGGLE 2 	/* tapping said times holds key until tapped once again */
+#define ONESHOT_TIMEOUT 2500 	/* time (in ms) befoer the one shot key is released */
 
 /*
 	Issues with MOD TAP
@@ -94,7 +72,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LEADER_TIMEOUT 340
 #define LEADER_NO_TIMEOUT
 
-#define TAPPING_TOGGLE 3  // TT layer shift momentary OR toggle ON/OFF if tapped thrice
+#define TAPPING_TOGGLE 2  			// TT layer shift momentary OR toggle ON/OFF if tapped thrice
+
+
+
+// Dynamic Macro Recording
+#define DYNAMIC_MACRO_USER_CALL		// stop recording without reset key
+#define DYNAMIC_MACRO_NO_NESTING    // prevents recording recursive macros 
+									//  PS: macro 1 in macro2 and vice versa is still possible
+
+
 
 // #define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX
 // #define UNICODE_KEY_LNX
