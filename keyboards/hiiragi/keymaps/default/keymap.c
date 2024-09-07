23715f5c3ba57c66d640e1edc0b12bb5eef585a0
Bharath <bharathsoftdev@gmail.com>
2024-09-07 15:44:30 +0530

    add keypad dot. git ingore for compiled uf2 files

--- keyboards/hiiragi/keymaps/default/keymap.c
+++ keyboards/hiiragi/keymaps/default/keymap.c
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_KEYPAD] = LAYOUT_ortho_4x10(
         KC_MUTE,     KC_MPRV,       KC_MPLY,    KC_MNXT,    KC_VOLU,                   KC_PSLS,     KC_7,       KC_8,       KC_9,       KC_PAST,
-        KC_LGUI,     KC_LALT,       KC_LSFT,    KC_LCTL,    KC_VOLD,                   XXXXXXX,     KC_4,       KC_5,       KC_6,       KC_PMNS,
+        KC_LGUI,     KC_LALT,       KC_LSFT,    KC_LCTL,    KC_VOLD,                   KC_DOT,      KC_4,       KC_5,       KC_6,       KC_PMNS,
         tgl_boot,    reset_kb,      tg_lower,   tg_raise,   kp_toggle,                 KC_0,        KC_1,       KC_2,       KC_3,       KC_PPLS,
         XXXXXXX,    _______,        lowers_bs,  KC_SPC,     _______,                   _______,     ent_sft,     mo_raise,    _______,    XXXXXXX
     ),
